a849776312e8c8aa6e547289b9ccb3dae82c3fe0
Jacob Evan Shreve <jacob@shreve.io>
2020-02-02 12:23:48 -0500

    Break out some code to different files

---
+++ roku/discover.go
@@ -0,0 +1,54 @@
+package roku
+
+import (
+	"net"
+	"bytes"
+	"log"
+	"regexp"
+)
+
+const ssdpAddr = "239.255.255.250:1900"
+const searchRequest =
+`M-SEARCH * HTTP/1.1
+Host: 239.255.255.250:1900
+Man: \"ssdp:discover\"
+ST: roku:ecp\n\n`
+
+func Discover() (Client, error) {
+
+	client := Client{}
+
+	// Bind a local UDP socket to listen for responses
+	sock, err := net.ListenPacket("udp", ":0")
+	if err != nil { return client, err }
+	defer sock.Close()
+
+	// Write the search request to the SSDP address
+	destAddr, err := net.ResolveUDPAddr("udp", ssdpAddr)
+	if err != nil { return client, err }
+	sock.WriteTo([]byte(searchRequest), destAddr)
+
+	for {
+
+		// Read in responses (UDP datagrams typically 8kb)
+		buf := make([]byte, 8192)
+		_, _, err := sock.ReadFrom(buf)
+		if err != nil { return client, err }
+
+		// Validate response from Roku Device
+		if ! bytes.Contains(buf, []byte("uuid:roku:ecp")) {
+			log.Println("This response doesn't look like it came from a Roku device. Skipping.")
+			continue
+		}
+
+		// Search for the location header and pull out the URL
+		re := regexp.MustCompile(`(?i)location: (.*)\r\n`)
+		address := re.FindSubmatch(buf)
+		if len(address) < 2 {
+			log.Println("Unable to parse device address from response. Skipping.")
+			continue
+		}
+
+		return Connect(string(address[1]))
+	}
+}
--- roku/roku.go
+++ roku/roku.go
@@ -1,24 +1,44 @@
 package roku
 
 import (
-	"net"
-	"net/http"
-	"bytes"
-	"regexp"
-	"log"
-	"fmt"
-	"io/ioutil"
 	"encoding/xml"
 )
 
-type Client struct {
-	address string
-	client *http.Client
-}
+const (
+	Home = "Home"
+	Rev = "Rev"
+	Fwd = "Fwd"
+	Play = "Play"
+	Select = "Select"
+	Left = "Left"
+	Right = "Right"
+	Down = "Down"
+	Up = "Up"
+	Back = "Back"
+	InstantReplay = "InstantReplay"
+	Info = "Info"
+	Backspace = "Backspace"
+	Search = "Search"
+	Enter = "Enter"
+	FindRemote = "FindRemote"
+	VolumeDown = "VolumeDown"
+	VolumeMute = "VolumeMute"
+	VolumeUp = "VolumeUp"
+	PowerOff = "PowerOff"
+	ChannelUp = "ChannelUp"
+	ChannelDown = "ChannelDown"
+	InputTuner = "InputTuner"
+	InputHDMI1 = "InputHDMI1"
+	InputHDMI2 = "InputHDMI2"
+	InputHDMI3 = "InputHDMI3"
+	InputHDMI4 = "InputHDMI4"
+	InputAV1 = "InputAV1"
+)
 
 type AppList struct {
 	Apps []App `xml:"app"`
 }
+
 type ActiveApp struct {
 	XMLName xml.Name `xml:"active-app"`
 	App App `xml:"app"`
@@ -81,111 +101,4 @@ func (a *App) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	}
 	a.Name = s
 	return nil
-}
-
-func (c *Client) Get(path string) ([]byte, error) {
-	url := c.address + path
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil { return []byte{}, err }
-	resp, err := c.client.Do(req)
-	if err != nil { return []byte{}, err }
-	return ioutil.ReadAll(resp.Body)
-}
-
-func (c *Client) Post(path string) error {
-	url := c.address + path
-	req, err := http.NewRequest("POST", url, nil)
-	if err != nil {
-		return err
-	}
-	_, err = c.client.Do(req)
-	return err
-}
-
-func (c *Client) Apps() []App {
-	body, err := c.Get("query/apps")
-	if err != nil { panic(err) }
-	al := AppList{}
-	xml.Unmarshal(body, &al)
-	return al.Apps
-}
-
-func (c *Client) ActiveApp() App {
-	body, err := c.Get("query/active-app")
-	if err != nil { panic(err) }
-	aa := ActiveApp{}
-	xml.Unmarshal(body, &aa)
-	return aa.App
-}
-
-func (c *Client) DeviceInfo() RokuInfo {
-	body, err := c.Get("query/device-info")
-	if err != nil { panic(err) }
-	info := RokuInfo{}
-	fmt.Println(string(body))
-	xml.Unmarshal(body, &info)
-	return info
-}
-
-func (c *Client) Keyup(key string) error {
-	return c.Post("keyup/" + key)
-}
-
-func (c *Client) Keydown(key string) error {
-	return c.Post("keydown/" + key)
-}
-
-func (c *Client) Keypress(key string) error {
-	return c.Post("keypress/" + key)
-}
-
-func (c *Client) Launch(appId int) error {
-	return c.Post(fmt.Sprintf("launch/%d", appId))
-}
-
-func (c *Client) Install(appId int) error {
-	return c.Post(fmt.Sprintf("install/%d", appId))
-}
-
-const ssdpAddr = "239.255.255.250:1900"
-const searchRequest = "M-SEARCH * HTTP/1.1\nHost: 239.255.255.250:1900\nMan: \"ssdp:discover\"\nST: roku:ecp\n\n"
-
-func Connect(addr string) (Client, error) {
-	return Client{addr, &http.Client{}}, nil
-}
-
-func Search() (Client, error) {
-
-	// Bind a local UDP socket to listen for responses
-	sock, err := net.ListenPacket("udp", ":0")
-	if err != nil { panic(err) }
-	defer sock.Close()
-
-	// Write the search request to the SSDP address
-	destAddr, err := net.ResolveUDPAddr("udp", ssdpAddr)
-	sock.WriteTo([]byte(searchRequest), destAddr)
-
-	for {
-
-		// Read in responses (UDP datagrams typically 8kb)
-		buf := make([]byte, 8192)
-		_, _, err := sock.ReadFrom(buf)
-		if err != nil { panic(err) }
-
-		// Validate response from Roku Device
-		if ! bytes.Contains(buf, []byte("uuid:roku:ecp")) {
-			log.Println("This response doesn't look like it came from a Roku device. Skipping.")
-			continue
-		}
-
-		// Search for the location header and pull out the URL
-		re := regexp.MustCompile(`(?i)location: (.*)\r\n`)
-		address := re.FindSubmatch(buf)
-		if len(address) < 2 {
-			log.Println("Unable to parse device address from response. Skipping.")
-			continue
-		}
-
-		return Connect(string(address[1]))
-	}
 }
